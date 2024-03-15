d60e964fd6b71a09926a674eca5e987f1d324951
frankelsammy <frankelsammy31@gmail.com>
2024-03-15 10:16:38 -0400

    Create shell script for running the code to be used by cron

---
+++ App/FantasyGolf/run.sh
@@ -0,0 +1,2 @@
+cd ~/FantasyGolf
+/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java @/var/folders/ch/sxhxbs9565x95rpfg11fq3y00000gn/T/cp_divc1tgi003ja4k9i4wcmv7bt.argfile Data 
