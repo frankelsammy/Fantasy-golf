b4571132dade746ef8805c88596af7c7c13c7ff7
frankelsammy <frankelsammy31@gmail.com>
2024-03-29 11:24:50 -0400

    Cut is now handled automatically, no need to pass the cutline

--- App/data/golf.py
+++ App/data/golf.py
@@ -13,7 +13,7 @@ import SECRET
 
 def results():
     #change number at end of string to get results for specific tournament 
-    url = "https://golf-leaderboard-data.p.rapidapi.com/leaderboard/641"
+    url = "https://golf-leaderboard-data.p.rapidapi.com/leaderboard/518"
 
     headers = {
         'x-rapidapi-host': "golf-leaderboard-data.p.rapidapi.com",
@@ -49,7 +49,7 @@ def results():
         l.append([players['last_name'], players['position'], players['status']])
 
 
-    with open('/Users/sammyfrankel/FantasyGolf/App/data/leaderboard2', 'w') as outfile:
+    with open('/Users/sammyfrankel/FantasyGolf/App/data/leaderboard', 'w') as outfile:
         for item in l:
             outfile.write(item[0] + ": " +  str(item[1]) + ": " + item[2] + '\n')
 
