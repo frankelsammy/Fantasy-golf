219b28a52bf4a9652e6ac76c9fa9ea98f7eac5e1
Sammy Frankel <frankelsammy31@gmail.com>
2026-01-11 16:33:55 -0500

    auto

--- fantasy-results-app/app/page.tsx
+++ fantasy-results-app/app/page.tsx
@@ -2,70 +2,18 @@
 import { Box, Flex, HStack, Link, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import LeaderboardTable from "@/components/leaderboard";
+import Navbar from "@/components/navbar";
 
-const Links = ["Overall Leaderboard", "Scoring"];
+const Links = ["Home", "Overall Leaderboard", "Scoring"];
 
-export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
+const page = () => {
   return (
     <div>
-      <div>
-        <Box bg="blue.800" px={4} color="yellow.400">
-          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-            {/* Logo / Brand */}
-            <Box fontWeight="bold" fontSize="xl">
-              Fantasy Golf Competition
-            </Box>
-
-            {/* Desktop Links */}
-            <HStack spacing={8} alignItems={"center"} display={{ base: "none", md: "flex" }}>
-              {Links.map((link) => (
-                <Link
-                  key={link}
-                  px={2}
-                  py={1}
-                  rounded={"md"}
-                  _hover={{ bg: "blue.700" }}
-                  href={`/${link.toLowerCase()}`}
-                >
-                  {link}
-                </Link>
-              ))}
-            </HStack>
-
-            {/* Mobile Hamburger */}
-            <IconButton
-              size={"md"}
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              aria-label={"Open Menu"}
-              display={{ md: "none" }}
-              onClick={isOpen ? onClose : onOpen}
-            />
-          </Flex>
-
-          {/* Mobile Menu */}
-          {isOpen ? (
-            <Box pb={4} display={{ md: "none" }}>
-              <Stack as={"nav"} spacing={4}>
-                {Links.map((link) => (
-                  <Link
-                    key={link}
-                    px={2}
-                    py={1}
-                    rounded={"md"}
-                    _hover={{ bg: "blue.700" }}
-                    href={`/${link.toLowerCase()}`}
-                  >
-                    {link}
-                  </Link>
-                ))}
-              </Stack>
-            </Box>
-          ) : null}
-        </Box>
-      </div>
+      <Navbar />
+      <img src='/open.png' alt='Open' style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '30%', paddingTop: '20px' }} />
       <LeaderboardTable />
     </div>
-  );
+  )
 }
+
+export default page
---
+++ fantasy-results-app/app/scoring/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react'
+import { Box, Flex, HStack, Link, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Links = ["Overall Leaderboard", "Scoring"];
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
