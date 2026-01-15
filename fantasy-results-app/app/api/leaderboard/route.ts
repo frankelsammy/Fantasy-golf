c8da205b4f2dc348f435a586a2fac84c24dc7e4f
Sammy Frankel <frankelsammy31@gmail.com>
2026-01-14 20:27:56 -0500

    cache api call

--- fantasy-results-app/app/api/leaderboard/route.ts
+++ fantasy-results-app/app/api/leaderboard/route.ts
@@ -1,11 +1,23 @@
 import clientPromise from "../../../lib/mongodb";
-
+let cachedLeaderboard: any = null;
+let lastFetched = 0;
+const CACHE_TTL = 100_000;
 export async function GET() {
   try {
+    const now = Date.now();
+
     const client = await clientPromise;
     const db = client.db("FantasyGolf");
-
+    if (cachedLeaderboard && now - lastFetched < CACHE_TTL) {
+      return new Response(JSON.stringify(cachedLeaderboard), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      })
+    }
     const leaderboard = await db.collection("League").findOne({ title: "leaderboard" });
+
+    cachedLeaderboard = leaderboard;
+    lastFetched = now;
 
     return new Response(JSON.stringify(leaderboard), {
       status: 200,
