7d535dfd2b7e0fbc377d33c0145aad6ad9a99d57
frankelsammy <72928526+frankelsammy@users.noreply.github.com>
2026-02-10 12:23:27 -0500

    Update config.py

--- App/FantasyGolf/main/config.py
+++ App/FantasyGolf/main/config.py
@@ -6,7 +6,15 @@ class TOURNAMENT(Enum):
     OPEN_CHAMPIONSHIP = 3
     PGA = 4
 
+class MODE(Enum):
+    PROD = 1
+    TESTING = 2
+
 #Select which tournament is currently being run
 current_tournament = TOURNAMENT.OPEN_CHAMPIONSHIP
 
 CURRENT_ROUND = 1
+
+#Select mode
+CURRENT_MODE = MODE.TESTING
+
