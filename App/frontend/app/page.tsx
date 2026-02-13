5f226ce6e66fdf1c078a4698d9489045dcc557fe
frankelsammy <72928526+frankelsammy@users.noreply.github.com>
2026-02-13 13:03:43 -0500

    Make Mobile Friendly Leaderboard
    * Made better looking leaderbaoard that works on mobile
    
    * fixed CUT
    
    * auto

--- App/frontend/app/page.tsx
+++ App/frontend/app/page.tsx
@@ -1,7 +1,8 @@
 import { Box, Divider, Flex, HStack, Link, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { getLeaderboardData } from "@/lib/getLeaderboardData";
-import LeaderboardTable from "@/components/leaderboard";
+
+import LeaderboardChooser from "@/components/LeaderboardChooser";
 import Navbar from "@/components/navbar";
 
 <Divider />
@@ -19,7 +20,7 @@ const page = async () => {
       <Navbar Links={Links} />
       <img src='/open.png' alt='Open' style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '30%', paddingTop: '20px' }} />
       <Divider mt={4} borderColor="gray.700" borderWidth="1px"/>
-      <LeaderboardTable initialData={data} />
+      <LeaderboardChooser initialData={data} />
     </div>
   )
 }
